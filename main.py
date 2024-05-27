372733e915c315495e349293f8dcad1dc0ab75f9
Gonpachiro Kamaboko <tomusalexandru12@gmail.com>
2024-05-27 16:50:57 +0000

    frist part

---
+++ main.py
@@ -0,0 +1,9 @@
+def lungimeString():
+    try:
+        text=input("Introduceti text:")
+        lungime=len(text)
+        print(f"Lungimea stringului este: {lungime}")
+    except ValueError as e:
+        print(e)
+
+lungimeString()
